8d706af5b7a66d20be19bcff1a395c96a89c045a
Camila Vilarinho <camilaavilarinho@gmail.com>
2017-03-17 14:52:12 -0300

    update server

--- index.js
+++ index.js
@@ -11,7 +11,12 @@ const imageSearchController = require('./controllers/imageSearch');
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended: true}))
 
-app.listen(5000, () => console.log('Webhook server is listening, port 5000'));
+app.set('port', (process.env.PORT || 5000))
+
+app.listen(app.get('port'), function() {
+    console.log('running on port', app.get('port'))
+})
+//app.listen(5000, () => console.log('Webhook server is listening, port 5000'));
 
 app.get('/', verificationController);
 app.post('/', messageWebhookController);
