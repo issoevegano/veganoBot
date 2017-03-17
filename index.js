cb0e6f11fcef2b5e34ea5d8f678b9835f5ee8c12
Camila Vilarinho <camilaavilarinho@gmail.com>
2017-03-16 22:20:01 -0300

    changing to use nlp

--- index.js
+++ index.js
@@ -1,33 +1,44 @@
 'use strict'
-
 const express = require('express')
 const bodyParser = require('body-parser')
-const request = require('request')
+
 const app = express()
 
-app.set('port', (process.env.PORT || 5000))
+const verificationController = require('./controllers/verification');
+const messageWebhookController = require('./controllers/messageWebhook');
 
-// Process application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({extended: false}))
-
-// Process application/json
 app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended: true}))
+
+app.listen(5000, () => console.log('Webhook server is listening, port 5000'));
+
+app.get('/', verificationController);
+app.post('/', messageWebhookController);
+
+
+
+
+/*app.set('port', (process.env.PORT || 5000))*/
+
+/*const request = require('request')*/
+
+
 
 // Index route
-app.get('/', function (req, res) {
+/*app.get('/', function (req, res) {
   res.send('Hello world, I am a chat bot')
-})
+})*/
 
 // for Facebook verification
-app.get('/webhook/', function (req, res) {
+/*app.get('/webhook/', function (req, res) {
   if (req.query['hub.verify_token'] === 'my_voice_is_my_password_verify_me') {
     res.send(req.query['hub.challenge'])
   }
   res.send('Error, wrong token')
-})
+})*/
 
 // Spin up the server
-app.listen(app.get('port'), function() {
+/*app.listen(app.get('port'), function() {
   console.log('running on port', app.get('port'))
 })
 
@@ -122,3 +133,4 @@ function sendGenericMessage(sender) {
     }
   })
 }
+*/
